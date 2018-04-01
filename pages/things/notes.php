898d075eba7091016608749fe29a5b2ebccff84a
System Administrator <root@v1020-wn-194-98.campus-dynamic.uwaterloo.ca>
2018-04-01 16:49:00 -0400

    2018-04-01: w2018 notes update

--- pages/things/notes.php
+++ pages/things/notes.php
@@ -20,17 +20,17 @@
 <p>When typing notes in HTML is suitable for one of my classes, I'll put them here. </p>
 
 <ul>
-	<li><a href="/pages/notes/PSYCH232/notes-psych232.html">PSYCH 232 - Psychology of Evil [Winter 2015]</a> (Completed)</li>
+	<li><a href="/pages/notes/PSYCH232/notes-psych232.html">PSYCH 232 - Psychology of Evil [Winter 2015]</a> (Complete)</li>
 	<li><a href="/pages/notes/PSYCH232/notes-psych232-baumeistertext.html">PSYCH 232 - Psychology of Evil Text Notes [Winter 2015]</a> - Evil: Inside Human Violence and Cruelty by Roy F. Baumeister. Notes for the chapters I took the time to write notes for.</li>
-	<li><a href="/pages/notes/PSYCH218/PSYCH218Notes.html">PSYCH 218 - Psychology of Death and Dying [Fall 2015]</a> (Completed)</li>
+	<li><a href="/pages/notes/PSYCH218/PSYCH218Notes.html">PSYCH 218 - Psychology of Death and Dying [Fall 2015]</a> (Complete)</li>
 	<li><a href="/pages/notes/PSYCH253/PSYCH253Notes.html">PSYCH 253 - Social Psychology [Fall 2015]</a> (Stopped after Lecture 6 because the prof went too fast and it was easier to just add notes to the slides).</li>
 	<li><a href="/pages/notes/ENGL108A/ENGL108A_notes.php">ENGL 108A - The Superhero [Spring 2016]</a></li>
-	<li><a href="/pages/notes/PSYCH353/PSYCH353_notes.php">PSYCH 353 - Social Cognition [Spring 2016]</a> - At one point, I stopped caring about format since I handwrote the notes.</li>
+	<li><a href="/pages/notes/PSYCH353/PSYCH353_notes.php">PSYCH 353 - Social Cognition [Spring 2016]</a> - At one point, I stopped caring about format since I rewrote the notes.</li>
 	<li><a href="/pages/notes/ANTH100/ANTH100_notes.php">ANTH 100 - Intro to Anthropology [Spring 2017]</a> - Never bothered to edit this after class because I rewrote the notes with paper and pen.</li>
 	<li><a href="/pages/notes/SMF215/SMF215Notes.html">SMF 215 - Sexuality and Popular Culture [Winter 2018]</a> (ongoing)</li>
 	<li><a href="/pages/notes/LS101/LS101Notes.html">LS 101 - Introduction to Legal Studies [Winter 2018]</a> (ongoing)</li>
-	<li><a href="/pages/notes/PSYCH356/PSYCH356Notes.html">PSYCH 356 - Personality [Winter 2018]</a> (ongoing) - Not enrolled in this course, so notes won't be that great.</li>
-	<li><a href="/pages/notes/PSYCH357/PSYCH357Notes.html">PSYCH 357 - Psychology of Good [Winter 2018]</a> (ongoing)</li>
+	<li><a href="/pages/notes/PSYCH356/PSYCH356Notes.html">PSYCH 356 - Personality [Winter 2018]</a> (Incomplete) - Wasn't actually enrolled, skipped a lecture to study, then never went again. 3-hour evening class was draining!</li>
+	<li><a href="/pages/notes/PSYCH357/PSYCH357Notes.html">PSYCH 357 - Psychology of Good [Winter 2018]</a> (Complete)</li>
 </ul>
 
 
